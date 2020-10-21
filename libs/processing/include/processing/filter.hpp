60164dd5de3bfa6917460c59f818e89378629c65
LucaRitz <ritz.luca@gmail.com>
2020-10-21 18:14:57 +0200

    init project

---
+++ libs/processing/include/processing/filter.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "macro_definition.hpp"
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+#include <utility>
+
+namespace cv {
+    class Mat;
+}
+
+using cv::Mat;
+using std::unordered_map;
+using std::vector;
+using std::pair;
+using std::hash;
+using Dimension = pair<int, int>;
+
+namespace proc {
+    struct hash_pair {
+        template <class T1, class T2>
+        size_t operator()(const pair<T1, T2>& p) const
+        {
+            auto hash1 = hash<T1>{}(p.first);
+            auto hash2 = hash<T2>{}(p.second);
+            return hash1 ^ hash2;
+        }
+    };
+}
+
+using Filters = unordered_map<Dimension, Mat, proc::hash_pair>;
+
+namespace proc {
+
+    class EXPORT Filter {
+    public:
+        Filter(double lowerBound, double upperBound);
+
+        Mat operator*(Mat toFilter);
+
+    private:
+        Filters _filter;
+        double _lowerBound;
+        double _upperBound;
+
+        Mat findAndCreateFilterIfNotExist(const Mat& toFilter);
+
+        static vector<Mat> splitByChannel(const Mat& src);
+        static Mat mergeChannels(const vector<Mat>& mats);
+        static Mat buildFilterOfFrequencyBand(int width, int height, double lowerBound, double upperBound);
+    };
+}
---
+++ libs/processing/include/processing/laplace_pyramid.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <vector>
+#include "macro_definition.hpp"
+
+namespace cv {
+    class Mat;
+}
+
+using t_level = unsigned short;
+using std::vector;
+using cv::Mat;
+
+namespace proc {
+    class Filter;
+
+    class EXPORT LaplacePyramid {
+    public:
+        explicit LaplacePyramid(cv::Mat& image, t_level levels=6);
+        LaplacePyramid(const LaplacePyramid& other) = default;
+
+        Mat reconstruct();
+        Mat at(t_level level);
+        t_level levels();
+
+        LaplacePyramid norm(int max);
+        LaplacePyramid operator+(const LaplacePyramid& other);
+        LaplacePyramid operator*(const vector<double>& factor);
+        LaplacePyramid operator*(Filter& filter);
+
+    private:
+        LaplacePyramid() = default;
+        vector<cv::Mat> _pyramid;
+    };
+}
